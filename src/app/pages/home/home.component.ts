6747a6dd8fa7fb64cf9a1dd486e1d4b263addc5a
Victor A. Barbosa <victor@wetune.com.br>
2025-11-12 15:10:28 -0300

    init commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   standalone: true
 })
 export class HomeComponent {
+
+  private router = inject(Router);
+
+  public HomeComponent() {
+
+  }
+
+  iniciarCalculo(session: string): void {
+    this.router.navigateByUrl('calculo');
+  }
 
 }
