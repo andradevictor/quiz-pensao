83f3cf143f52d3afdb9711c9b053a10907b0390a
Victor A. Barbosa <victor@wetune.com.br>
2025-11-12 14:40:23 -0300

    init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { QuestionsComponent } from './pages/questions/questions.component';
+
+export const routes: Routes = [
+    { path: '', component: QuestionsComponent }
+];
